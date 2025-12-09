bb5cc8d5d80e57366b4d6f0349122e7e43c6c3bc
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-12-09 15:52:50 +0100

    Added read_selections method to get all the selections of the DB

---
+++ business/goncourt.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Goncourt
+"""
+
+from dataclasses import dataclass, field
+from typing import Optional
+
+from daos.dao import Dao
+from daos.selection_dao import SelectionDao
+from models.selection import Selection
+
+## PRENDRE EN COMPTE QU'ON N'EST QUE LE 03/09 et que le président sélectionne les romans pour les deux autres tours
+
+@dataclass
+class Goncourt:
+    """Couche métier de l'application de gestion du prix Goncourt,
+    reprenant les cas d'utilisation et les spécifications fonctionnelles
+    """
+
+    selections: list[Selection]
+
+    def add_selection(self, selection: Selection) -> None:
+        """Ajout du cours course à la liste des cours."""
+        self.selections.append(selection)
+
+
+    def read_selections(self):
+        with Dao.connection.cursor() as cursor:
+            sql = "SELECT id FROM selection"
+            cursor.execute(sql,)
+            record = cursor.fetchall()
+        if record is not None:
+            self.selections.append(SelectionDao.read(record['id']))
+        else:
+            self.selections = []
